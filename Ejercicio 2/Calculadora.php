69ae5bf7dcf52eec63725f7478bb96a9f2665ffa
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-02-23 11:32:29 -0600

    termine la calculadora

--- Ejercicio 2/Calculadora.php
+++ Ejercicio 2/Calculadora.php
@@ -1,28 +1,39 @@
 <?php
-
-if(isset($_POST['numero1']) && isset($_POST['numero2']))
-{
-    $numero1 = $_POST['numero1'];
-    $numero2 = $_POST['numero2'];
-    $total = $numero1 + $numero2;
-    echo "La suma es " . $total;
+class matematica {
+public static function sumar($num1,$num2){
+  $suma=$num1+$num2;
+  return $suma;
 }
-// class matematica {
-//     public static function sumar($num1,$num2){
-//     $suma=$num1+$num2;
-//     return $suma;
-//     }
-//     public static function restar($num1,$num2){
-//     $resta=$num1-$num2;
-//         return $resta;
-//     }
-//     public static function multiplicar($num1,$num2){
-//     $multi=$num1*$num2;
-//     return $multi;
-//     }
-//     public static function dividir($num1,$num2){
-//     $divi=$num1/$num2;
-//     return $divi;
-//     }    
-// }
+  public static function restar($num1,$num2){
+ $resta=$num1-$num2;
+   return $resta;
+}
+public static function multiplicar($num1,$num2){
+$multi=$num1*$num2;
+return $multi;
+}
+public static function dividir($num1,$num2){
+$divi=$num1/$num2;
+ return $divi;
+    }    
+ }
+
+require_once "Calculadora.php";
+if(isset($_REQUEST['calcular'])){
+
+    $n1=$_REQUEST['numero1'];
+    $n2=$_REQUEST['numero2'];
+    $op=$_REQUEST['opciones'];
+
+    switch($op){
+
+        case 0:echo "$n1 + $n2 = ".matematica::sumar($n1,$n2);break;
+        case 1:echo "$n1 - $n2 = ".matematica::restar($n1,$n2);break;
+        case 2:echo "$n1 * $n2 = ".matematica::multiplicar($n1,$n2);break;
+        case 3:echo "$n1 / $n2 = ".matematica::dividir($n1,$n2);break;
+
+
+    }
+
+}
 ?>
