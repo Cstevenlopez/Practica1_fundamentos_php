c89dd561252da0cd2550a69140a5fcbe1696557b
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-02-25 09:18:56 -0600

    Haciendo probas del array

--- Ejercicio 4/Arreglos.php
+++ Ejercicio 4/Arreglos.php
@@ -1,20 +1,28 @@
-<html>
-<head>
-    <title>Arreglo</title>
-</head>
-<body>
-<form method="post" action="Arreglos.php">
-<input type="text" name="numero">
-
-<input type="submit" name="generar" value="Generar">
-    
-</form>
-</body>
-</html>
 <?php
-$rand = range(1,20);
-shuffle($rand);
-foreach ($rand as $val) {
-echo "$val ";
+$n=$_GET["valorn"];
+echo "Estos son $n numeros aleatorios entre 1 y 100 <br />";
+$total=0;
+$min=null;
+$max=null;
+$mr[] = array();
+while ($cantidad<=$n -1){
+    $rand=rand(1,100);
+
+    $mr = array($rand);
+    
+    echo "Numero aleatorio $rand <br />";
+    $total+=$rand;
+    if (is_null($min) || $rand<$min) {
+        $min = $rand; // guardo el menor
+    }
+    if (!is_null($max) || $rand>$max) {
+        $max = $rand; // guardo el mayor
+    }
+ $cantidad ++;
 }
+echo "La suma de los n&uacute;meros generados es $total <br />";
+echo "El promedio es ".($total/$n)."<br />";
+/*echo "El menor es $min <br />";
+echo "El mayor es $max <br />";*/
+echo "Valor que mas se repite : ".$mr[0]; 
 ?>
