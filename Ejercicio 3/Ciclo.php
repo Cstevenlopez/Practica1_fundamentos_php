f4e761588240181d1b9068c733deef920f57ad93
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-02-28 15:55:13 -0600

    realize cambios en el ciclo

--- Ejercicio 3/Ciclo.php
+++ Ejercicio 3/Ciclo.php
@@ -1,54 +1,50 @@
 <?php
-function recoge($var, $m = "")
-{
-    if (!isset($_REQUEST[$var])) {
-        $tmp = (is_array($m)) ? [] : "";
-    } elseif (!is_array($_REQUEST[$var])) {
-        $tmp = trim(htmlspecialchars($_REQUEST[$var], ENT_QUOTES, "UTF-8"));
-    } else {
-        $tmp = $_REQUEST[$var];
-        array_walk_recursive($tmp, function (&$valor) {
-            $valor = trim(htmlspecialchars($valor, ENT_QUOTES, "UTF-8"));
-        });
-    }
-    return $tmp;
+if(!$_POST) {
+    header('Location:DosParametros.html');
+
 }
+$nombre = $_POST['nombre'];
+$número = $_POST['número'];
+$ciclo = 1;
 
-$columnas = recoge("columnas");
-
-$columnasOk = false;
-
-$columnasMinimo = 1;
-$columnasMaximo = 1000;
-
-if ($columnas == "") {
-    print "  <p class=\"aviso\">No ha escrito el número de columnas.</p>\n";
-    print "\n";
-} elseif (!is_numeric($columnas)) {
-    print "  <p class=\"aviso\">No ha escrito el número de columnas como número.</p>\n";
-    print "\n";
-} elseif (!ctype_digit($columnas)) {
-    print "  <p class=\"aviso\">No ha escrito el número de columnas "
-        . "como número entero positivo.</p>\n";
-    print "\n";
-} elseif ($columnas < $columnasMinimo || $columnas > $columnasMaximo) {
-    print "  <p class=\"aviso\">El número de columnas debe estar entre "
-        . "$columnasMinimo y $columnasMaximo.</p>\n";
-    print "\n";
-} else {
-    $columnasOk = true;
-}
-
-if ($columnasOk) {
-    print "  <table class=\"conborde\">\n";
-    print "    <tbody>\n";
-    print "      <tr>\n";
-    for ($i = 1; $i <= $columnas; $i++) {
-        print "        <td>$i</td>\n";
-    }
-    print "      </tr>\n";
-    print "    </tbody>\n";
-    print "  </table>\n";
-    print "\n";
-}
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabla de registro</title>
+</head>
+<body>
+<h1>Nombre de la persona registrada: <?php echo $nombre ?></h1>
+    
+    <body>
+        <table border="1";
+            <thead>
+            <tr><th> Cantidad de Registro: <td> <?php echo $número ?></td></th>                                      
+            </tr>
+            
+        </thead>
+        </table>
+        </body>
+    <?php       
+   
+if($número > 10000){
+    echo "<p>{$ciclo} {$nombre} " . "</p>";
+}
+
+else{
+    while($ciclo <= $número){
+        
+       echo "<p>{$ciclo} {$nombre} " . "</p>";
+        $ciclo++;
+     }
+
+}
+    ?>
+    
+   
+  
+</body>
+</html>
