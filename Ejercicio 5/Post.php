370e81d669f13c3e98e79d23df8c1d1f2352b37b
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-03-01 15:04:11 -0600

    codigo para generar nombres

--- Ejercicio 5/Post.php
+++ Ejercicio 5/Post.php
@@ -1,7 +1,20 @@
 <?php
+session_start();
+$nombres = array();
+if(isset($_POST['nombre']))
+{
+    if(isset($_SESSION['nombres'])){
+         $nombres = $_SESSION['nombres'];
+         $nombres[] = $_POST['nombre'];
+         $_SESSION['nombres'] = $nombres;
+     } else {
+         array_push($_SESSION['nombres'], $_POST['nombre']);
+     }
+}
 
-$nombre = $_POST['nombre'];
-
-echo $nombre;
-
+$valores = $_SESSION["nombres"];
+foreach($valores as $val)
+{
+    echo $val."<br>" ;
+} 
 ?>
