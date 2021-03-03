849a69229798f9757a6ea0a30a0e9cf5bce0378c
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-03-03 13:05:02 -0600

    cree el formulario

---
+++ Ejercicio 7/logica/conexion.php
@@ -0,0 +1,17 @@
+<?php
+/*
+$host = "localhost";
+$user = "root";
+$clave = "";
+$bd  = "practica";
+$conectar = mysqli_connect($host,$user,$clave,$bd);
+
+
+$conectar = new PDO('mysql:host=localhost;dbname=practica','root','');
+*/
+
+$db = new PDO('mysql:host=127.0.0.1;dbname=practica','root','');
+$db->setAttribute (PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+$filas = $db->query('SELECT * FROM datos;');
+
+?>
---
+++ Ejercicio 7/logica/guardar.php
@@ -0,0 +1,45 @@
+<?php
+ require 'conexion.php';
+  
+ $nombre  = $_POST['nombre'];
+ $apellido  = $_POST['apellido'];
+ $edad = $_POST['edad'];
+ $sexo = $_POST['sexo'];
+ $direccion = $_POST['direccion'];
+
+/*
+$insertar = "INSERT INTO datos VALUES (' $nombre ','$apellido',' $edad','$sexo',' $direccion') ";
+
+$query = mysqli_query($conectar, $insertar);
+*/
+
+$query = <<<SQL
+INSERT INTO datos(nombre,apellido,edad,sexo,direccion)
+VALUES (:nombre,:apellido,:edad,:sexo,:direccion)
+SQL;
+$statement = $db->prepare($query);
+$params = [
+    'nombre' => $nombre,
+    'apellido' => $apellido,
+    'edad' => $edad,
+    'sexo' => $sexo,
+    'direccion' => $direccion,
+];
+
+$statement->execute($params);
+
+if($statement){
+
+   echo "<script> alert('correcto');
+    location.href = '../index.html';
+   </script>";
+
+}else{
+    echo "<script> alert('incorrecto');
+    location.href = '../index.html';
+    </script>";
+}
+
+
+
+?>
